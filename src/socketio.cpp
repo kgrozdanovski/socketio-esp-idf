226face03313463e2b28ce10512bab692f4f8d06
kgrozdanovski <qlimax.republic@gmail.com>
2022-01-17 21:26:28 +0100

    Restructure project

---
+++ src/socketio.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file socketio.cpp
+ * 
+ * @author Kristijan Grozdanovski (kgrozdanovski7@gmail.com)
+ * 
+ * @brief SocketIO client library for ESP-IDF framework.
+ * 
+ * @version 1
+ * 
+ * @date 2022-01-10
+ * 
+ * @copyright Copyright (c) 2022, Kristijan Grozdanovski
+ * All rights reserved.
+ * 
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. 
+ */
+
+#include "socketio.h"
+
+void app_main() {
+    
+}
