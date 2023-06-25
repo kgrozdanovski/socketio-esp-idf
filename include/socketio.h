dd884c91fef79f1c258e4d6ab6bf91067ae56bae
Dominik Völkel <voelkel.dominik@gmail.com>
2023-06-25 13:45:44 +0200

    Add kconfig for EIO version

--- include/socketio.h
+++ include/socketio.h
@@ -1,26 +1,27 @@
 /**
  * @file socketio.h
- * 
+ *
  * @author Kristijan Grozdanovski (kgrozdanovski7@gmail.com)
- * 
+ *
  * @brief SocketIO client library for ESP-IDF framework.
- * 
+ *
  * @version 1
- * 
+ *
  * @date 2022-01-10
- * 
+ *
  * @copyright Copyright (c) 2022, Kristijan Grozdanovski
  * All rights reserved.
- * 
+ *
  * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. 
+ * LICENSE file in the root directory of this source tree.
  */
 
 #ifndef __SOCKETIO_H
 #define __SOCKETIO_H
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 #include <stdio.h>
@@ -43,16 +44,15 @@ extern "C" {
 #include "socketio_types.h"
 #include "utility.h"
 
-/*
- * ----------------------------------------------------------
- *
- *              Defines and component constants.
- *
- * ----------------------------------------------------------
- */
+    /*
+     * ----------------------------------------------------------
+     *
+     *              Defines and component constants.
+     *
+     * ----------------------------------------------------------
+     */
 
-// todo: make these kconfig configurable
-#define EIO_VERSION 4
+#define EIO_VERSION (CONFIG_EIO_VERSION)
 #define SIO_DEFAULT_NAMESPACE "/"
 #define SIO_DEFAULT_URL_PATH "/socket.io"
 #define SIO_DEFAULT_MAX_CONN_RETRIES 3
@@ -68,58 +68,58 @@ extern "C" {
 #define ASCII_RS ""
 #define ASCII_RS_INDEX = 30
 
-/**
- * @brief Module tag used in log.
- * 
- */
-static const char *SIO_TAG = "sio";
+    /**
+     * @brief Module tag used in log.
+     *
+     */
+    static const char *SIO_TAG = "sio";
 
-/**
- * @brief SocketIO event base declaration
- * 
- */
-ESP_EVENT_DECLARE_BASE(SIO_EVENT);
+    /**
+     * @brief SocketIO event base declaration
+     *
+     */
+    ESP_EVENT_DECLARE_BASE(SIO_EVENT);
 
-/*
- * ----------------------------------------------------------
- *
- *              Internal function prototypes.
- *
- * ----------------------------------------------------------
- */
+    /*
+     * ----------------------------------------------------------
+     *
+     *              Internal function prototypes.
+     *
+     * ----------------------------------------------------------
+     */
 
-static esp_err_t socketio_http_handshake(sio_client_t *sio_client);
-static esp_err_t socketio_compile_url(const char *url, const char *server_address);
-static void socketio_polling(void* pvParameters);
-static esp_err_t socketio_parse_message_queue(sio_client_t* socketio_client, char* content);
-static esp_err_t socketio_send_pong(sio_client_t* socketio_client);
-static esp_err_t socketio_send_pong_http(sio_client_t* socketio_client);
-static bool socketio_is_message(char* token, size_t* token_length);
+    static esp_err_t socketio_http_handshake(sio_client_t *sio_client);
+    static esp_err_t socketio_compile_url(const char *url, const char *server_address);
+    static void socketio_polling(void *pvParameters);
+    static esp_err_t socketio_parse_message_queue(sio_client_t *socketio_client, char *content);
+    static esp_err_t socketio_send_pong(sio_client_t *socketio_client);
+    static esp_err_t socketio_send_pong_http(sio_client_t *socketio_client);
+    static bool socketio_is_message(char *token, size_t *token_length);
 
-/*
- * ----------------------------------------------------------
- *
- *      Prototypes for functions of the public API.
- *
- * ----------------------------------------------------------
- */
+    /*
+     * ----------------------------------------------------------
+     *
+     *      Prototypes for functions of the public API.
+     *
+     * ----------------------------------------------------------
+     */
 
-/**
- * @brief Initialize the SocketIO client with default values
- * 
- * @param sio_client 
- * @param server_address 
- */
-void socketio_client_init(sio_client_t *sio_client, const char *server_address);
+    /**
+     * @brief Initialize the SocketIO client with default values
+     *
+     * @param sio_client
+     * @param server_address
+     */
+    void socketio_client_init(sio_client_t *sio_client, const char *server_address);
 
-/**
- * @brief 
- * 
- * @param sio_client 
- * @param connect_retries 
- * @return esp_err_t 
- */
-esp_err_t socketio_begin(sio_client_t *sio_client, uint8_t connect_retries);
+    /**
+     * @brief
+     *
+     * @param sio_client
+     * @param connect_retries, 0 = infinite
+     * @return esp_err_t
+     */
+    esp_err_t socketio_begin(const sio_client_t *sio_client, const uint8_t connect_retries);
 
 #ifdef __cplusplus
 }
